50f49c4edc96e0237f7dda45b5cdd1caa9ad7304
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-30 23:44:37 +0000

    reformatted test on how RestAssured functions is declared

--- src/test/java/getRequest/GetWeather.java
+++ src/test/java/getRequest/GetWeather.java
@@ -12,7 +12,7 @@ public class GetWeather {
 
         String apiKey = System.getenv("API_KEY");
 
-        Response resp =  get("http://api.openweathermap.org/data/2.5/weather?q=Paris,France&appid="+apiKey);
+        Response resp =  get("http://api.openweathermap.org/data/2.5/weather?q=Paris,France&appid=" + apiKey);
 
         int statusCode = resp.getStatusCode();
 
@@ -32,7 +32,6 @@ public class GetWeather {
 
         System.out.println("Status code is "+body);
         System.out.println("Response time "+resp.getTime());
-
     }
 
 }
