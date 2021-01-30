ac34331c81e0304df4262eddad287db9ed76d247
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-30 23:43:21 +0000

    reformatted test on how

--- src/test/java/getRequest/GetWeather.java
+++ src/test/java/getRequest/GetWeather.java
@@ -1,6 +1,6 @@
 package getRequest;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,13 +12,27 @@ public class GetWeather {
 
         String apiKey = System.getenv("API_KEY");
 
-        Response resp =  RestAssured.get("http://api.openweathermap.org/data/2.5/weather?q=Paris,France&appid=" + apiKey );
+        Response resp =  get("http://api.openweathermap.org/data/2.5/weather?q=Paris,France&appid="+apiKey);
 
         int statusCode = resp.getStatusCode();
 
         System.out.println("Status code is "+statusCode);
 
         Assert.assertEquals(statusCode,200);
+    }
+
+    @Test
+    public void testBody() {
+
+        String apiKey = System.getenv("API_KEY");
+
+        Response resp =  get("http://api.openweathermap.org/data/2.5/weather?q=Paris,France&appid=" + apiKey );
+
+        String body = resp.asString();
+
+        System.out.println("Status code is "+body);
+        System.out.println("Response time "+resp.getTime());
+
     }
 
 }
